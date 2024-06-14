b9d67fc3ed4f82005190dbb2103fd96f330bf76a
Akshay Gowda S <akshaygowda1110@gmail.com>
2024-06-14 17:49:10 +0530

    trial

---
+++ Hello.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+# include<math.h>
+
+
+int main() {
+    int a,b,c;
+    printf("enter the number = ");
+    scanf("%d",&a);
+    printf("enter the number = ");
+    scanf("%d",&b);
+    printf("enter the number = ");
+    scanf("%d",&c);
+    printf("average = %d",(a+b+c)/3);
+    return 0;
+} 
